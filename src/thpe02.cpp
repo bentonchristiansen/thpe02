e67b15a1a21d2f09672e9d3564cafd1a37ca24cf
Benton <benton.christiansen@mines.sdsmt.edu>
2025-11-17 14:15:02 -0700

    Main Function

--- src/thpe02.cpp
+++ src/thpe02.cpp
@@ -7,14 +7,21 @@ int main(int argc, char**argv)
     std::ofstream fout1;
     std::ofstream fout2;
 
-    //ensures correct argument count, files opened succesfully.
+    //ensures correct argument count, files opened succesfully, ends program
+    //if any unsuccessful.
     checkArgc(argc);
     openInFile(fin, argv[1]);
     openOutFile(fout1, argv[2]);
     openOutFile(fout2, argv[2]);
 
+    //reads words into tracker.
     readWords(fin, wordTracker);
 
+    //prints phrases and counters into file 1 and 2.
+    wordTracker.printPhrases(fout1);
+    wordTracker.printCounters(fout2);
+
+    //closes files, returns.
     fin.close();
     fout1.close();
     fout2.close();
