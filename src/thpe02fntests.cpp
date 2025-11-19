2f80340e707494524589678671fb7794e0cd818e
Benton <benton.christiansen@mines.sdsmt.edu>
2025-11-19 08:45:35 -0700

    cleanWord TestCases

--- src/thpe02fntests.cpp
+++ src/thpe02fntests.cpp
@@ -2,3 +2,33 @@
 #include "thpe02.h"
 #include <catch2/catch_test_macros.hpp>
 
+TEST_CASE("cleanWord - no punctuation")
+{
+    std::string word = "word";
+    cleanWord(word);
+    REQUIRE(word == "word");
+}
+TEST_CASE("cleanWord - front punctuation")
+{
+    std::string word = ".word";
+    cleanWord(word);
+    REQUIRE(word == "word");
+}
+TEST_CASE("cleanWord - back punctuation")
+{
+    std::string word = "word,";
+    cleanWord(word);
+    REQUIRE(word == "word");
+}
+TEST_CASE("cleanWord - front and back punctuation")
+{
+    std::string word = ",word.";
+    cleanWord(word);
+    REQUIRE(word == "word"); 
+}
+TEST_CASE("cleanWord - multiple front and back punctuation")
+{
+    std::string word = ",,.,..!word!!,.,.!!!";
+    cleanWord(word);
+    REQUIRE(word == "word");   
+}
