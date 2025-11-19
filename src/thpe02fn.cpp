c7b95eec72d91a9430d581501246f68395300909
Benton <benton.christiansen@mines.sdsmt.edu>
2025-11-19 08:41:14 -0700

    clean word?

--- src/thpe02fn.cpp
+++ src/thpe02fn.cpp
@@ -6,15 +6,15 @@ void checkArgc(int argc)
     // statement and triggers error flag.
     if (argc != 4)
     {
-        std::cout << "Usage: thpe01 inputfile outputfile1 outputfile2" 
+        std::cout << "Usage: thpe01 inputfile outputfile1 outputfile2"
                   << std::endl;
         std::cout << "inputfile - text file containing the words to be "
                   << " processed." << std::endl;
         std::cout << "outputfile1 - filename for the file that contains words"
-                  << " listed alphabetically by their first character." 
+                  << " listed alphabetically by their first character."
                   << std::endl;
         std::cout << "outputfile2 - filename for the file that contains words"
-                  << " listed in descending order by their frequency count." 
+                  << " listed in descending order by their frequency count."
                   << std::endl;
         exit(1);
     }
@@ -23,16 +23,27 @@ void checkArgc(int argc)
 
 void cleanWord(std::string &word)
 {
+    // while first character is punctuation, erase first character.
+    while (std::ispunct(word.front()))
+    {
+        word.erase(0, 1);
+    }
+
+    // while last character is punctuation, erase last character.
+    while (std::ispunct(word.back()))
+    {
+        word.pop_back();
+    }
 
     return;
 }
 
 void intoTracker(std::string &word, StringTracker &wordTracker)
 {
-    //Increments count, then adds string to tracker.
-    //This prevents an if statement because no error will be thrown if the word 
-    //has not been added yet, and will be added after. Same thing the other way
-    //around.
+    // Increments count, then adds string to tracker.
+    // This prevents an if statement because no error will be thrown if the word
+    // has not been added yet, and will be added after. Same thing the other way
+    // around.
     wordTracker.incrementCount(word);
     wordTracker.addString(word);
 
@@ -71,16 +82,16 @@ void openOutFile(std::ofstream &fout, char fileName[])
 
 void readWords(std::ifstream &fin, StringTracker &wordTracker)
 {
-    //Intializing temp string.
+    // Intializing temp string.
     std::string temp;
 
-    //while fin can go into temp string
-    while(fin >> temp)
+    // while fin can go into temp string
+    while (fin >> temp)
     {
-        //cleans temp string
+        // cleans temp string
         cleanWord(temp);
 
-        //puts into tracker or increments.
+        // puts into tracker or increments.
         intoTracker(temp, wordTracker);
     }
 
